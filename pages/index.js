32493fb69a32869dab43daa88b951966e5a51068
janniks <jannik.siebert@gmail.com>
2021-02-14 19:52:04 +0100

    Update webpage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,74 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const updateUrl = (e) => setUrl(e.target.value);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>OpenMedium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <span className={styles.gradient}>OpenMedium</span>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>An API for sharing Medium articles</p>
+        <div className={styles.example}>
+          <code className={styles.code}>
+            {"curl http://localhost:3000/api/open?url=<MEDIUM_URL>"}
+          </code>
+        </div>
 
+        <h2>Try it out!</h2>
+        <div className={styles.try}>
+          <div className={styles.input}>
+            <label>
+              Medium URL <input type="text" id="url" onChange={updateUrl} />
+            </label>
+          </div>
+          <div className={styles.submit}>
+            <button onClick={() => alert("todo")}>Get URL</button>
+          </div>
+        </div>
+
+        <h2>Extensions</h2>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>iOS Shortcut &rarr;</h3>
+            <p>Installable iOS shortcut</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Bookmarklet</h3>
+            <p>Drag this link to your bookmarks</p>
           </a>
+        </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <h2>API Reference</h2>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.reference}>
+          <h3>/api/open</h3>
+          <p>
+            Returns the short <strong>t.co</strong> link for a given Medium
+            article
+          </p>
+          <h4>Parameters</h4>
+          <p>
+            <code className={styles.code}>url</code> the Medium article URL
+            <i> (as a form, JSON, or query parameter; everything works)</i>
+          </p>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Made by{" "}
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
       </footer>
     </div>
-  )
+  );
 }

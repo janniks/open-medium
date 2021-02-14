a0aeadc55dfd1e78ab511b73db4b5800009b9450
janniks <jannik.siebert@gmail.com>
2021-02-14 18:19:19 +0100

    Add serverless api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Twitter = require("twitter");
+
+const client = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_SECRET,
+});
+
+// https://stackoverflow.com/a/43467144/1139105
+function isValidMediumUrl(string) {
+  try {
+    const url = new URL(string);
+    return url.hostname === "medium.com" && url.protocol === "https:";
+  } catch (e) {
+    return false;
+  }
+}
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  try {
+    const url = req.query.url || req.body.url;
+
+    if (!url) {
+      throw Error("'url' query parameter missing");
+    }
+
+    if (!isValidMediumUrl(url)) {
+      throw Error("Not a valid Medium URL");
+    }
+
+    // todo: pad length if too short
+    client.post(
+      "statuses/update",
+      { status: `${new Date().toUTCString()}, this article was read ${url}` },
+      function (err, tweet) {
+        if (err) throw err;
+        res.json(tweet.entities.urls[0].url);
+      }
+    );
+  } catch (error) {
+    res.status(400).json({ error: error.message || error });
+  }
+};
